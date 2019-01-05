ee2bf81c0ca7aa61b9000f2972c8da5dde4a902f
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-06 01:08:07 +0200

    theme palette config

--- src/muiTheme.js
+++ src/muiTheme.js
@@ -3,16 +3,30 @@ import { createMuiTheme } from '@material-ui/core/styles'
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#663A92',
+      light: '#344DBC',
+      main: '#192b7f',
+      dark: '#192B81',
+      contrastText: '#ffffff',
     },
     secondary: {
-      main: '#FAFAFA',
+      light: '#344DBC',
+      main: '#35c1ce',
+      dark: '#2AA4AF',
+      contrastText: '#ffffff',
     },
   },
 
   typography: {
     useNextVariants: true,
-  }
+  },
+
+  props: {
+    // withWidth component ⚛️
+    MuiWithWidth: {
+      // Initial width property
+      initialWidth: 'lg', // Breakpoint being globally set 
+    },
+  },
 })
 
 export default theme
