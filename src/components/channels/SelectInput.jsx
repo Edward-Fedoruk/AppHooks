b235aa6f4a3adba77371deae93a1da0f8f348d48
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-10 23:56:02 +0200

    outlined select

---
+++ src/components/channels/SelectInput.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+
+const styles = theme => ({
+  formControl: {
+    minWidth: "300px",
+  },
+
+})
+
+class SelectInput extends React.Component {
+  state = {
+    labelWidth: 0,
+  }
+
+  componentDidMount() {
+    this.setState({
+      labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth,
+    })
+  }
+
+
+
+  render() {
+    const { classes, options, name, styles, handleChange, region } = this.props
+
+    return (
+      <FormControl 
+        variant="outlined" 
+        className={classes.formControl}
+        style={styles}
+      >
+        <InputLabel
+          ref={ref => {
+            this.InputLabelRef = ref
+          }}
+          htmlFor={`outlined-${name}`}
+        >
+          {name}
+        </InputLabel>
+        <Select
+          value={region}
+          onChange={handleChange}
+          input={
+            <OutlinedInput
+              labelWidth={this.state.labelWidth}
+              name={name}
+              id={`outlined-${name}`}
+            />
+          }
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {options.map((option, index) => (
+            <MenuItem key={index} value={option}>
+              {option[0]}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    )
+  }
+}
+
+SelectInput.propTypes = {
+  classes: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  options: PropTypes.array,
+  styles: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(SelectInput)
