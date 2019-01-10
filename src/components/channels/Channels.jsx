227ebba2f38a6c01d758c7cb7b08756811e40418
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-11 00:04:28 +0200

    conditional rendering

--- src/components/channels/Channels.jsx
+++ src/components/channels/Channels.jsx
@@ -2,14 +2,13 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { withStyles } from '@material-ui/core/styles'
-import TopBar from '../TopBar'
 import withNavigation from '../withNavigation'
 import { compose } from "redux"
+import CreateChannel from './CreateChannel'
+import AllChannels from './AllChannels'
 
 const styles = () => ({
-  contentWrap: {
-    padding: "25px 35px"
-  }
+
 })
 
 export class Channels extends Component {
@@ -23,15 +22,9 @@ export class Channels extends Component {
     const { classes } = this.props
     return (
       <Fragment>
-        <TopBar 
-          buttonText="Create New App"
-          title="Your Channels Apps"
-          onButtonClick={this.createChannel}
-          button
-        />
-          <div className={classes.contentWrap}>
-
-          </div>
+        {true 
+          ? <AllChannels />
+          : <CreateChannel />}
       </Fragment>
     )
   }
