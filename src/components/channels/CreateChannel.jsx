b1648c49d3d47e4383cdc6abc7593666b10ac115
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-11 00:01:18 +0200

    channel's creation form

---
+++ src/components/channels/CreateChannel.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator'
+import { compose } from 'redux'
+import SubmitButton from '../Authentication/SubmitButton'
+import SelectInput from "./SelectInput"
+import { CountryRegionData } from "react-country-region-selector"
+import FormTitle from '../FormTitle'
+
+const styles = () => ({
+  formWrap: {
+    width: "60%",
+    margin: "auto",
+    marginTop: "50px",
+  },
+
+  textField: {
+    width: "100%",
+    marginTop: "70px"
+  },
+
+  title: {
+    fontWeight: "500",
+    fontSize: "25px",
+    color: "#5A5B5F",
+    marginBottom: "25px"
+  },
+
+  p: {
+    fontSize: "16px",
+    color: "#7C7D81"
+  }
+  
+})
+
+export class CreateChannel extends Component {
+  state = {
+    name: "", 
+    region: ""
+  }
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+  }
+  
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  onChange = input => e => this.setState({ [input]: e.target.value })
+
+  render() {
+    const { classes } = this.props
+    const { name, region } = this.state
+    return (
+      <div className={classes.formWrap} >
+        <FormTitle 
+          paragraph={"Create a Channels app to generate your unique credentials. You can create as many apps as you need."}
+          title={"Create your Channels App"}
+        />
+        
+        <ValidatorForm onSubmit={this.onSubmit}>
+          <TextValidator
+            label="Name your app (required)"
+            name="name"
+            autoFocus
+            variant="outlined"
+            placeholder="MyApp"
+            onChange={this.onChange('name')}
+            className={classes.textField}
+            value={name}
+            margin="normal"
+            validators={['required']}
+            errorMessages={['this field is required']}
+          />
+
+          <SelectInput 
+            options={CountryRegionData}
+            name="region"
+            region={region}
+            handleChange={this.handleChange}
+            styles={{width: "100%", marginTop: "35px"}}
+          />
+
+          <SubmitButton 
+            text={"Log in"}
+            styles={{width: "250px", display: "block", margin: "84px auto 20px auto"}}
+          />
+
+        </ValidatorForm>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = {
+  
+}
+
+export default compose(
+  withStyles(styles),
+  connect(null, mapDispatchToProps)
+)(CreateChannel)
