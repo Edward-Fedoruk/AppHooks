355f4ff522675ec4df4d1db218df8df0990af0c1
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-11 00:02:09 +0200

    title and description of a form

---
+++ src/components/FormTitle.jsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = () => ({
+  title: {
+    fontWeight: "500",
+    fontSize: "25px",
+    color: "#5A5B5F",
+    marginBottom: "25px"
+  },
+
+  p: {
+    fontSize: "16px",
+    color: "#7C7D81"
+  },
+})
+
+const FormTitle = ({ classes, titleStyle, pStyle, title, paragraph }) => {
+  return (
+    <Fragment>
+      <Typography 
+        className={classes.title} 
+        variant="h3" 
+        align="left"
+        style={titleStyle}
+      >
+        {title}
+      </Typography>
+
+      <Typography 
+        className={classes.p} 
+        align="left"
+        style={pStyle}
+      >
+       { paragraph }
+      </Typography>
+    </Fragment>
+  )
+}
+
+FormTitle.propTypes = {
+  classes: PropTypes.object.isRequired,
+  titleStyle: PropTypes.object,
+  pStyle: PropTypes.object
+}
+
+export default withStyles(styles)(FormTitle)
