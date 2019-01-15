0ae3debc2dcae2e93c17105bb5a76ecf25be9c7d
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-15 20:27:31 +0200

    Updates

---
+++ precache-manifest.97c52298c259424d3eb8916ce61edc69.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e1b91a722c859c7aea0412fab2affc6",
+    "url": "/AppHooks/static/media/LogoIcon.0e1b91a7.svg"
+  },
+  {
+    "revision": "2a51c447008285afb2bf",
+    "url": "/AppHooks/static/css/main.cfd971f8.chunk.css"
+  },
+  {
+    "revision": "a11bda68f3912caadacc",
+    "url": "/AppHooks/static/js/1.a11bda68.chunk.js"
+  },
+  {
+    "revision": "000bc896836c93f2586b",
+    "url": "/AppHooks/static/js/runtime~main.000bc896.js"
+  },
+  {
+    "revision": "1e1f712ccbf429eb57eef8e2cd43200e",
+    "url": "/AppHooks/static/media/Logo.1e1f712c.svg"
+  },
+  {
+    "revision": "2a51c447008285afb2bf",
+    "url": "/AppHooks/static/js/main.2a51c447.chunk.js"
+  },
+  {
+    "revision": "6c352158a5a5862dce40205e7af908e6",
+    "url": "/AppHooks/static/media/GitHub.6c352158.svg"
+  },
+  {
+    "revision": "83f606bc7a42b17ca63ebbd5cc0e23dc",
+    "url": "/AppHooks/static/media/Google.83f606bc.svg"
+  },
+  {
+    "revision": "60ae52309ba8e4af3abf8d6b01896165",
+    "url": "/AppHooks/static/media/User.60ae5230.svg"
+  },
+  {
+    "revision": "f2bb3e32801cbc6912dfe460e09dd78e",
+    "url": "/AppHooks/static/media/QuestionMark.f2bb3e32.svg"
+  },
+  {
+    "revision": "67b081a7d7950db39ef7b0c5cf64e7ad",
+    "url": "/AppHooks/index.html"
+  }
+];
