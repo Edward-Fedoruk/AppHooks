66132eb99fec9e7ff2ebe957bd8d40bb85d183f0
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-15 19:23:56 +0200

    added deep channels removing

--- src/reducers/channelsEntities.js
+++ src/reducers/channelsEntities.js
@@ -1,5 +1,4 @@
 import * as types from '../actions/types'
-import { stat } from 'fs';
 
 const initialState = {
   entities: {
@@ -11,43 +10,71 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case types.SET_CHANNELS:
+      return {
+        ...state,
+        entities: action.channels.entities,
+        result: action.channels.result
+      }
+    case types.SET_STAGES: 
+      return {
+        ...state,
+        entities: {
+          ...state.entities,
+          ...action.payload.entities,
+          stages: { ...state.entities.stages, ...action.payload.entities.stages }
+        }
+      }
+    case types.REMOVE_CHANNEL:
+      const channels = state.entities.channels
+      const stagesIds = channels[action.channelId].stages
+      const stages = state.entities.stages
+      const endpoints = state.entities.endpoints
+      const newEndpoints = Object.keys(endpoints)
+        .filter(key => endpoints[key].application_id === action.channelId)
+        .reduce((result, current) => {
+          result[current] = endpoints[current]
+          return result
+        }, {})
+      const newStages =  Object.keys(stages)
+        .filter(key => !stagesIds.includes(parseInt(key)))
+        .reduce((result, current) => {
+          result[current] = stages[current]
+          return result
+        }, {})
+      const newChannels = Object.keys(channels)
+        .filter(key => key !== action.channelId)
+        .reduce((result, current) => {
+          result[current] = channels[current]
+          return result
+        }, {}) 
 
-  case types.SET_CHANNELS:
-    return {
-      ...state,
-      entities: action.channels.entities,
-      result: action.channels.result
-    }
-  case types.REMOVE_CHANNEL:
-    const channels = state.entities.channels
-    return {
-      ...state,
-      entities: {
-        ...state.entities,
-        channels: Object.keys(channels)
-          .filter(key => key !== action.channelId)
-          .reduce((result, current) => {
-            result[current] = channels[current]
-            return result
-          }, {})
-      },
-      result:{
-        data: state.result.filter((id) => id !== action.channelId),
-      } 
-    }
-  case types.ADD_CHANNEL:
-    const { entities, result } = state
-    return {
-      ...state,
-      entities: {
-        ...entities,
-        ...action.payload.entities,
-        channels: { ...entities.channels, ...action.payload.entities.channels }
-      },
-      result: { data: [...result, action.payload.result] }
-    }
+      console.log(newEndpoints, newStages, newChannels)
+      return {
+        ...state,
+        entities: {
+          ...state.entities,
+          endpoints: newEndpoints,
+          stages: newStages,
+          channels: newChannels 
+        },
+        result:{
+          data: state.result.filter((id) => id !== action.channelId),
+        } 
+      }
+    case types.ADD_CHANNEL:
+      const { entities, result } = state
+      return {
+        ...state,
+        entities: {
+          ...entities,
+          ...action.payload.entities,
+          channels: { ...entities.channels, ...action.payload.entities.channels }
+        },
+        result: { data: [...result, action.payload.result] }
+      }
 
-  default:
-    return state
+    default:
+      return state
   }
 }
