75dca0446df3e70b7adc5fba1fe260d59fe7e6ad
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-22 21:25:47 +0200

    added PropTypes

--- src/components/Placeholder.jsx
+++ src/components/Placeholder.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { withStyles } from '@material-ui/core'
-import Typography from '@material-ui/core/Typography'
-import classNames from 'classnames'
+import React from "react"
+import { withStyles } from "@material-ui/core"
+import Typography from "@material-ui/core/Typography"
+import classNames from "classnames"
+import PropTypes from "prop-types"
 
 const styles = () => ({
   placeholder: {
@@ -43,16 +44,24 @@ const styles = () => ({
   }
 })
 
-const Placeholder = ({ classes, title, subtitle, button, className, imgSrc }) => {
-  return (
-    <div className={classNames(classes.placeholder, className)}>
-      <img src={imgSrc} alt="stages"/>
-      <div className={classes.wrap}>
-        <Typography variant="h5" className={classes.title}>{ title }</Typography>
-        <Typography variant="subtitle2" className={classes.subtitle}>{ subtitle }<span className={classes.textButton}> { button }</span></Typography>
-      </div>
+const Placeholder = ({ classes, title, subtitle, button, className, imgSrc }) => (
+  <div className={classNames(classes.placeholder, className)}>
+    <img src={imgSrc} alt="stages"/>
+    <div className={classes.wrap}>
+      <Typography variant="h5" className={classes.title}>{ title }</Typography>
+      <Typography variant="subtitle2" className={classes.subtitle}>{ subtitle }<span className={classes.textButton}> { button }</span></Typography>
     </div>
-  )
+  </div>
+)
+
+
+Placeholder.propTypes = {
+  classes: PropTypes.object,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  button: PropTypes.string,
+  className: PropTypes.object,
+  imgSrc: PropTypes.string
 }
 
 export default withStyles(styles)(Placeholder)
