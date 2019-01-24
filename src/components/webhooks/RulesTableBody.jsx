2407e0595322c10558c6b93e8d0eb21fbf58b40f
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-01-24 14:04:59 +0200

    splited RulesTable

---
+++ src/components/webhooks/RulesTableBody.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableRow from "@material-ui/core/TableRow"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import IconButton from "@material-ui/core/IconButton"
+import { withStyles } from "@material-ui/core"
+
+const styles = () => ({
+  rulName: {
+    width: "100%",
+    color: "#192B7F",
+    fontSize: "14px"
+  },
+})
+
+const RulesTableBody = ({ page, data, rowsPerPage, classes, emptyRows }) => {
+  return (
+    <TableBody>
+      {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(rule => (
+        <TableRow>
+          <TableCell className={classes.rulName} >
+            {rule.name}
+          </TableCell>
+          <TableCell align="right" padding="none">
+            <IconButton aria-haspopup="true">
+              <MoreVertIcon />
+            </IconButton>
+          </TableCell>
+        </TableRow>
+      ))}
+      
+      {emptyRows > 0 && (
+        <TableRow style={{ height: 49 * emptyRows }}>
+          <TableCell colSpan={6} />
+        </TableRow>
+      )}
+    </TableBody>
+  )
+}
+
+RulesTableBody.propTypes = {
+  page: PropTypes.number,
+  data: PropTypes.array,
+  rowsPerPage: PropTypes.number,
+  classes: PropTypes.object,
+  emptyRows: PropTypes.number
+}
+
+export default withStyles(styles)(RulesTableBody)
---
+++ src/components/webhooks/RulesTableHead.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import TableCell from "@material-ui/core/TableCell"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Typography from "@material-ui/core/Typography"
+import { withStyles } from "@material-ui/core"
+import PropTypes from "prop-types"
+
+const styles = () => ({
+  colName: {
+    fontFamily: "Lato",
+    fontWeight: "bold",
+    fontSize: "14px"
+  }
+})
+
+const RulesTableHead = ({ classes }) => (
+  <TableHead>
+    <TableRow>
+      <TableCell>
+        <Typography 
+          className={classes.colName} 
+          color="primary"
+        >
+          Name
+        </Typography>
+      </TableCell>
+      <TableCell></TableCell>
+    </TableRow>
+  </TableHead>
+)
+
+RulesTableHead.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+
+export default withStyles(styles)(RulesTableHead)
