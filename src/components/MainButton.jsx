038c8f167087aa478800b6d2d0074afc70d6c9de
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-03 13:48:09 +0200

    custome green button

---
+++ src/components/MainButton.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Button from "@material-ui/core/Button"
+import { withStyles } from "@material-ui/core"
+import classNames from "classnames"
+
+const styles = () => ({
+  btn: {
+    textTransform: "capitalize",
+    color: "#fff",
+    fontSize: "16px",
+    background: "#35C1CE",
+
+    "&:hover": {
+      opacity: ".9",
+      background: "#192B81",
+    },
+  },
+})
+
+const MainButton = props => {
+  const { classes, className, children, ...rest } = props
+  return (
+    <Button 
+      size="large"
+      color="primary" 
+      variant="text"
+      className={classNames(classes.btn, className)}
+      {...rest}
+    >
+      {children}
+    </Button>
+  )
+}
+
+MainButton.propTypes = {
+  classes: PropTypes.object.isRequired, 
+  className: PropTypes.string, 
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+}
+
+export default withStyles(styles)(MainButton)
