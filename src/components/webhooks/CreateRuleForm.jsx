724c916c8b5e7ca547bdd4ab9a12e86f41a2f710
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-04 17:00:51 +0200

    extracted rule form to seperate component

---
+++ src/components/webhooks/CreateRuleForm.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { ValidatorForm } from "react-material-ui-form-validator"
+import FormAce from "./FormAce"
+import FormRuleName from "./FormRuleName"
+import MainButton from "../MainButton"
+import { withStyles } from "@material-ui/core"
+import { createRule } from "../../actions/rules"
+
+const styles = ({ breakpoints }) => ({
+  newRule: {
+    display: "flex",
+    margin: "20px auto",
+  },
+
+  inputWrap: {
+    display: "flex",
+    height: "25vw",
+    width: "100%",
+    maxWidth: "1061px",
+    margin: "33px auto",
+    justifyContent: "space-between",
+    
+    [breakpoints.down(768)]: {
+      flexDirection: "column",
+      height: "100%"
+    },
+  },
+})
+
+export class CreateRuleForm extends Component {
+  state = {
+    code: "",
+    inputCode: "",
+    name: ""
+  }
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired
+  }
+
+  submit = () => {
+    this.props.createRule({
+      name: this.state.name,
+      description: "",
+      input_type: "plain",
+      input: this.state.inputCode,
+      code: this.state.code
+    })
+  }
+
+  codeChange = code => this.setState({ code })
+  inputCodeChange = inputCode => this.setState({ inputCode })
+  nameChange = e => this.setState({ name: e.target.value })
+
+  render() {
+    const { classes } = this.props
+    const { code, inputCode, name } = this.state
+
+    return (
+      <ValidatorForm onSubmit={this.submit}>
+        <FormRuleName onChange={this.nameChange} value={name} />
+        <div className={classes.inputWrap}>
+          <FormAce onChange={this.codeChange} code={code} caption="Transformation code" />
+          <FormAce onChange={this.inputCodeChange} code={inputCode} caption="input data" /> 
+        </div>
+        <MainButton
+          className={classes.newRule}
+          type="submit"
+        >
+          Create New Rule
+        </MainButton>
+      </ValidatorForm>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  createRule: data => dispatch(createRule(data))
+})
+
+export default withStyles(styles)(connect(null, mapDispatchToProps)(CreateRuleForm))
