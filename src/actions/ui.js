dd6a09f4015106819933f9e4a3b94402bf48c067
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-04 16:49:30 +0200

    toggling edit form on action

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -8,6 +8,10 @@ export const toggleSnackbar = () => ({
   type: types.TOGGLE_SNACKBAR
 })
 
+export const toggleEditForm = () => ({
+  type: types.TOGGLE_EDIT_RULE_FORM
+})
+
 export const changeStage = (stage) => ({
   type: types.CHANGE_STAGE,
   stage
--- src/reducers/view.js
+++ src/reducers/view.js
@@ -4,6 +4,7 @@ const initialState = {
   open: false,
   snackbar: false,
   currentStage: 0,
+  editRuleForm: false
 }
 
 export default (state = initialState, action) => {
@@ -14,6 +15,8 @@ export default (state = initialState, action) => {
       return { ...state, snackbar: !state.snackbar }
     case types.CHANGE_STAGE:
       return { ...state, currentStage: action.stage }
+    case types.TOGGLE_EDIT_RULE_FORM: 
+      return { ...state, editRuleForm: !state.editRuleForm}
     default:
       return state
   }
