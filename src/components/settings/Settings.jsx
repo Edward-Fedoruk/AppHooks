e2a743e410c5b3b0eee7bec6ba3d3e606403278e
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-07 20:36:19 +0200

    error and confirm dialog

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -9,7 +9,9 @@ import GeneralSettings from "./GeneralSettings"
 import ChangePassword from "./ChangePassword"
 import ConnectedAccounts from "./ConnectedAccounts"
 import DeleteAccount from "./DeleteAccount"
-import { fetchUserSettings } from "../../actions/user"
+import { fetchUserSettings, deleteAccount } from "../../actions/user"
+import ErrorSnackbar from "../Authentication/ErrorSnackbar"
+import ConfirmDialog from "../ConfirmDialog"
 
 const styles = ({ breakpoints }) => ({
   contentWrap: {
@@ -23,9 +25,20 @@ const styles = ({ breakpoints }) => ({
 })
 
 export class Settings extends Component {
+  state = {
+    open: false
+  }
+
   static propTypes = {
     classes: PropTypes.object.isRequired,
     settings: PropTypes.object.isRequired
+  }
+
+  toggleDialog = () => this.setState(({open}) => ({open: !open}))
+
+  handleCloseWithAction = () => {
+    this.props.deleteAccount()
+    this.toggleDialog()
   }
 
   componentDidMount() {
@@ -33,19 +46,28 @@ export class Settings extends Component {
   }
 
   render() {
-    const { classes, settings } = this.props
+    const { classes, settings, error } = this.props
+    console.log(settings)
     return (
       <Fragment>
+        <ErrorSnackbar message={error.message}/>
+
+        <ConfirmDialog  
+          open={this.state.open}
+          handleCloseWithAction={this.handleCloseWithAction}
+          handleClose={this.toggleDialog}
+        />
+
         <TopBar title="Account Settings" />
         <div className={classes.contentWrap}>
           <GeneralSettings 
-            name={settings.name}
             company={settings.company}
+            name={settings.name}
             phone={settings.phone}
           />
           <ChangePassword />
           <ConnectedAccounts />
-          <DeleteAccount />
+          <DeleteAccount openDialog={this.toggleDialog} />
         </div>
       </Fragment>
     )
@@ -53,13 +75,14 @@ export class Settings extends Component {
 }
 
 const mapStateToProps = ({ userSettings }) => ({
-  settings: userSettings.settings
+  settings: userSettings.settings,
+  error: userSettings.error
 })
 
 const mapDispatchToProps = {
-  fetchUserSettings
+  fetchUserSettings,
+  deleteAccount
 }
-
 
 export default compose(
   withNavigation,
