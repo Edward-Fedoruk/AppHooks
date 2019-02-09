f38d9d963e7418d94c11f3c5c66aa3fe39c10cd5
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-09 12:46:56 +0200

    redundant data

--- src/components/users/SubUsers.jsx
+++ src/components/users/SubUsers.jsx
@@ -9,27 +9,6 @@ import { withStyles } from "@material-ui/core"
 import { fetchUsers } from "../../actions/subUsers"
 import AddUser from "./AddUser"
 
-let counter = 0
-function createData(email, name, role, privileges, active) {
-  counter += 1
-  return { id: counter, email, name, role, privileges, active }
-}
-
-const data = [
-  createData("Cupcake", "test1", "test2", "test3", "yes"),
-  createData("Donut", "test1", "test2", "test3", "test4"),
-  createData("Frozen yoghurt", "test1", "test2", "test3", "test4"),
-  createData("Gingerbread", "test1", "test2", "test3", "test4"),
-  createData("Honeycomb", "test1", "test2", "test3", "test4"),
-  createData("Ice cream sandwich", "test1", "test2", "test3", "test4"),
-  createData("Jelly Bean", "test1", "test2", "test3", "test4"),
-  createData("KitKat", "test1", "test2", "test3", "test4"),
-  createData("Lollipop", "test1", "test2", "test3", "test4"),
-  createData("Marshmallow", "test1", "test2", "test3", "test4"),
-  createData("Nougat", "test1", "test2", "test3", "test4"),
-  createData("Oreo", "test1", "test2", "test3", "test4"),
-]
-
 const styles = ({ breakpoints }) => ({
   contentWrap: {
     padding: "20px 26px 20px 13px",
