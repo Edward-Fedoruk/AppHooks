86ac027e505ca14e08cb9b14b7231ddffe2596a9
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-10 18:11:49 +0200

    removed placeholder text

--- src/components/ConfirmDialog.jsx
+++ src/components/ConfirmDialog.jsx
@@ -1,11 +1,7 @@
-/* eslint-disable react/jsx-one-expression-per-line */
 import React from "react"
 import PropTypes from "prop-types"
 import Dialog from "@material-ui/core/Dialog"
 import DialogActions from "@material-ui/core/DialogActions"
-import DialogContent from "@material-ui/core/DialogContent"
-import DialogContentText from "@material-ui/core/DialogContentText"
-import DialogTitle from "@material-ui/core/DialogTitle"
 import Button from "@material-ui/core/Button"
 
 export const ConfirmDialog = ({ open, handleClose, handleCloseWithAction }) => (
@@ -15,13 +11,6 @@ export const ConfirmDialog = ({ open, handleClose, handleCloseWithAction }) => (
     aria-labelledby="alert-dialog-title"
     aria-describedby="alert-dialog-description"
   >
-    <DialogTitle id="alert-dialog-title">Use googles location service?</DialogTitle>
-    <DialogContent>
-      <DialogContentText id="alert-dialog-description">
-        Let Google help apps determine location. This means sending anonymous location data toGoogle, even when no apps are running.
-      </DialogContentText>
-    </DialogContent>
-
     <DialogActions>
       <Button onClick={handleClose} color="primary">
         Disagree
