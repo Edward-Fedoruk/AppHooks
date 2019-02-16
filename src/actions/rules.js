169dd755dcbd2bd6b3d0869882d28d809a73b12e
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-16 16:16:07 +0200

    refactored with axios

--- src/actions/rules.js
+++ src/actions/rules.js
@@ -1,9 +1,18 @@
+import { compose } from "redux"
 import * as types from "./types"
-import { domain, setFetchSettings } from "./utils"
+import { initiateLoading, createError } from "./utils"
+import axios from "./utils"
 import { toggleEditForm } from "./ui"
 
+const throwRuleErr = createError("SET_RULES")
+const destructError = ({ response }) => response.data
+const handleError = dispatch => compose(
+  compose(dispatch, throwRuleErr),
+  destructError
+)
+
 export const setRules = recipes => ({
-  type: types.SET_RULES,
+  type: types.SET_RULES_SUCCESS,
   recipes,
 })
 
@@ -17,6 +26,7 @@ export const addRule = recipe => ({
   recipe,
 })
 
+
 export const editRuleInStore = (id, newRule) => ({
   type: types.EDIT_RULE,
   newRule,
@@ -29,78 +39,41 @@ export const deleteRuleFromStore = id => ({
 })
 
 export const fetchRules = () => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("GET", accessToken, null)
+  dispatch(initiateLoading("SET_RULES"))
 
-  fetch(`${domain}/recipes`, settings)
-    .then(response => response.json().then(json => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
-    .then((response) => {
-      console.log(response.data)
-      dispatch(setRules(response.data))
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  axios.get("/recipes")
+    .then(({ data: { data } }) => compose(dispatch, setRules)(data))
+    .catch(({ response: { data } }) => compose(dispatch, throwRuleErr)(data))
 }
 
 export const fetchRule = id => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("GET", accessToken, null)
-
-  fetch(`${domain}/recipes/${id}`, settings)
-    .then(response => response.json().then(json => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
-    .then((response) => {
-      console.log(response.data)
-      dispatch(setRule(response.data))
+  console.log("test")
+  axios.get(`/recipes/${id}`)
+    .then(({ data: { data } }) => {
+      dispatch(setRule(data))
       dispatch(toggleEditForm())
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch(handleError(dispatch))
 }
 
 export const createRule = data => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("POST", accessToken, JSON.stringify(data))
-
-  console.log(data)
-  fetch(`${domain}/recipes`, settings)
-    .then(response => response.json().then(json => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
-    .then((response) => {
-      console.log(response.data)
-      dispatch(addRule(response.data))
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  axios.post("/recipes", data)
+    .then(({ data: { data } }) => compose(dispatch, addRule)(data))
+    .catch(handleError(dispatch))
 }
 
 export const editRule = (id, data) => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("PUT", accessToken, JSON.stringify(data))
-
-  fetch(`${domain}/recipes/${id}`, settings)
-    .then(response => response.json().then(json => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
-    .then((response) => {
-      console.log(response.data, id)
-      dispatch(editRuleInStore(id, response.data))
+  axios.put(`/recipes/${id}`, data)
+    .then(({ data: { data } }) => {
+      console.log(data, id)
+      dispatch(editRuleInStore(id, data))
       dispatch(toggleEditForm())
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch(handleError(dispatch))
 }
 
 export const deleteRule = id => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("DELETE", accessToken, null)
-
-  fetch(`${domain}/recipes/${id}`, settings)
-    .then(response => (response.ok ? Promise.resolve() : Promise.reject()))
-    .then(() => {
-      dispatch(deleteRuleFromStore(id))
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  axios.delete(`/recipes/${id}`)
+    .then(() => compose(dispatch, deleteRuleFromStore)(id))
+    .catch(handleError(dispatch))
 }
