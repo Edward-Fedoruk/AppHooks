f568e5001615ff916194501ab0d4cb682648df58
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-22 22:41:32 +0200

    choose which error to show

--- src/reducers/errorHandler.js
+++ src/reducers/errorHandler.js
@@ -1,13 +1,23 @@
-
 export default (state = {}, action) => {
   const { type, error } = action
   const matches = /(.*)_(REQUEST|FAILURE)/.exec(type)
+  let errorMessages = ""
 
-  if (!matches) return state
+  if (!matches || error === undefined) return state
+
+  console.log(error)
+  if (error.errors) {
+    errorMessages = Object.values(error.errors).flat().join("\n")
+  } else if (error.message) {
+    errorMessages = error.message
+  } else {
+    errorMessages = "something went wrong"
+    console.log(error)
+  }
 
   const [, requestName, requestState] = matches
   return {
     ...state,
-    [requestName]: requestState === "FAILURE" ? error.message : "",
+    [requestName]: requestState === "FAILURE" ? errorMessages : "",
   }
 }
