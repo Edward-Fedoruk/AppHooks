f26d82d6379201d27950585c1ab3d0643d86302b
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-27 13:47:22 +0200

    refactored with axios

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,9 +1,13 @@
-import { setFetchSettings, domain } from "./utils"
+import axios, {
+  initiateLoading,
+  handleResponse,
+  createError,
+} from "./utils"
 import * as types from "./types"
 import { toggleSnackbar } from "./ui"
 
 export const setUserToStore = data => ({
-  type: types.SET_USER_SETTINGS,
+  type: types.SET_USER_SETTINGS_SUCCESS,
   data,
 })
 
@@ -13,65 +17,35 @@ export const throwError = error => ({
 })
 
 export const fetchUserSettings = () => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("GET", accessToken, null)
+  dispatch(initiateLoading("SET_USER_SETTINGS"))
 
-  fetch(`${domain}/profile`, settings)
-    .then(response => response.json().then(json => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
-    .then((response) => {
-      console.log(response.data)
-      dispatch(setUserToStore(response.data))
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  axios.get("/profile")
+    .then(handleResponse(dispatch, setUserToStore))
+    .catch(() => dispatch(createError("SET_USER_SETTINGS")))
 }
 
 export const changeUserSettings = data => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("PUT", accessToken, JSON.stringify(data))
-
-  fetch(`${domain}/profile`, settings)
-    .then(response => response.json().then(json => (response.ok ? Promise.resolve(json) : Promise.reject(json))))
-    .then((response) => {
-      console.log(response.data)
-      dispatch(setUserToStore(response.data))
-    })
-    .catch((err) => {
-      console.log(err)
-      dispatch(throwError(err))
+  axios.put("/profile", data)
+    .then(handleResponse(dispatch, setUserToStore))
+    .catch((response) => {
+      dispatch(throwError(response))
       dispatch(toggleSnackbar())
     })
 }
 
 export const changeUserPassword = data => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("PUT", accessToken, JSON.stringify(data))
-
-  fetch(`${domain}/profile/change-password`, settings)
-    .then(response => (!response.ok
-      ? response.json().then(json => Promise.reject(json))
-      : response))
-    .then((response) => {
-      console.log(response)
-    })
+  axios.put("/profile/change-password", data)
+    .then(response => console.log(response))
     .catch((err) => {
-      console.log(err)
       dispatch(throwError(err))
       dispatch(toggleSnackbar())
     })
 }
 
 export const deleteAccount = () => (dispatch) => {
-  const accessToken = localStorage.getItem("JWT")
-  const settings = setFetchSettings("DEL", accessToken, null)
-
-  fetch(`${domain}/profile`, settings)
-    .then((response) => {
-      console.log(response)
-    })
+  axios.delete("/profile")
+    .then(response => console.log(response))
     .catch((err) => {
-      console.log(err)
       dispatch(throwError(err))
       dispatch(toggleSnackbar())
     })
