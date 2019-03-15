e2fa526a0bcd0f1f3d0b5d5e12748d5550752a8f
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-15 22:42:07 +0200

    ajax deletion logic

--- src/actions/stage.js
+++ src/actions/stage.js
@@ -3,15 +3,16 @@ import * as types from "./types"
 import { stageSchema } from "./schemas"
 import axios from "./utils"
 import history from "../history"
+import { toggleCreateStageForm, changeStage } from "./ui"
 
 export const setStagesData = (payload, id) => ({
-  type: types.SET_STAGES,
+  type: types.CREATE_STAGE_SUCCESS,
   payload,
   id,
 })
 
 export const throwStageCreationError = error => ({
-  type: types.CREATE_STAGE_ERROR,
+  type: types.CREATE_STAGE_FAILURE,
   error,
 })
 
@@ -25,18 +26,35 @@ export const addStageId = id => ({
   id,
 })
 
+export const removeStage = (channelId, stageId) => ({
+  type: types.REMOVE_STAGE_SUCCESS,
+  channelId,
+  stageId,
+})
+
 export const createStage = (id, stageData) => (dispatch) => {
   axios.post(`/apps/${id}/stages`, stageData)
     .then(({ data: { data } }) => {
       const normalizedData = normalize(data, stageSchema)
       dispatch(setStagesData(normalizedData, id, data.id))
-
       if (history.location.pathname.split("/").reverse()[0] === `${id}`) {
         dispatch(addStageId(data.id))
+        dispatch(toggleCreateStageForm())
       }
     })
     .catch((er) => {
       console.log(er.response)
       dispatch(throwStageCreationError(er.message))
+    })
+}
+
+export const deleteStage = (channelId, stageId) => (dispatch) => {
+  axios.delete(`/apps/${channelId}/stages/${stageId}`)
+    .then(() => {
+      dispatch(removeStage(channelId, stageId))
+      dispatch(changeStage(0))
+    })
+    .catch((err) => {
+      console.log(err)
     })
 }
