46eb966a3cd8027ddd99f0d1bfb7f1438b9c974b
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-16 21:40:50 +0200

    form for changing stage name

---
+++ src/components/stages/EditStageName.jsx
@@ -0,0 +1,116 @@
+import React, { Component, Fragment } from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { withStyles } from "@material-ui/core/styles"
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator"
+import { compose } from "redux"
+import FormTitle from "../utils/FormTitle"
+import { editStageName } from "../../actions/stage"
+import MainButton from "../utils/MainButton"
+import ErrorSnackbar from "../utils/ErrorSnackbar"
+import { createErrorMessageSelector } from "../../actions/utils"
+import history from "../../history"
+
+const styles = () => ({
+  formWrap: {
+    width: "60%",
+    margin: "auto",
+    marginTop: "50px",
+  },
+
+  textField: {
+    width: "100%",
+    marginTop: "70px",
+  },
+
+  submitButton: {
+    margin: "25px auto",
+    display: "flex",
+  },
+
+  title: {
+    fontWeight: "500",
+    fontSize: "25px",
+    color: "#5A5B5F",
+    marginBottom: "25px",
+  },
+
+  p: {
+    fontSize: "16px",
+    color: "#7C7D81",
+  },
+
+})
+
+export class CreateStage extends Component {
+  state = {
+    name: "",
+  }
+
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    editStageName: PropTypes.func.isRequired,
+    errorMessage: PropTypes.string,
+  }
+
+  static defaultProps = {
+    errorMessage: "Something went wrong",
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    const { editStageName, stageId } = this.props
+    const channelId = history.location.pathname.split("/").reverse()[0]
+
+    editStageName(channelId, stageId)
+  }
+
+  handleChange = event => this.setState({ [event.target.name]: event.target.value })
+
+  onChange = input => e => this.setState({ [input]: e.target.value })
+
+  render() {
+    const { classes, errorMessage } = this.props
+    const { name } = this.state
+    return (
+      <Fragment>
+        <ErrorSnackbar message={errorMessage} />
+        <FormTitle title="Edit Stage" />
+
+        <ValidatorForm onSubmit={this.onSubmit}>
+          <TextValidator
+            label="Stage name"
+            name="name"
+            autoFocus
+            variant="outlined"
+            placeholder="MyStage"
+            onChange={this.onChange("name")}
+            className={classes.textField}
+            value={name}
+            margin="normal"
+            validators={["required"]}
+            errorMessages={["this field is required"]}
+          />
+
+          <MainButton className={classes.submitButton} type="submit">Confirm</MainButton>
+
+        </ValidatorForm>
+      </Fragment>
+    )
+  }
+}
+
+const errorSelector = createErrorMessageSelector(["CREATE_ENDPOINT"])
+
+const mapStateToProps = ({ errorHandler }) => ({
+  errorMessage: errorSelector(errorHandler),
+})
+
+const mapDispatchToProps = dispatch => ({
+  editStageName: (channelId, stageId, newName) => dispatch(editStageName(channelId, stageId, newName)),
+})
+
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps, mapDispatchToProps)
+)(CreateStage)
