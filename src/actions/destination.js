84ba4e9a6f9bf9f21440361f13da47dcb33aa2ab
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-24 19:11:08 +0200

    destination actions with types

---
+++ src/actions/destination.js
@@ -0,0 +1,80 @@
+import { compose } from "redux"
+import * as types from "./types"
+import axios, {
+  handleErrorResponse,
+  createError,
+  handleResponse,
+  initiateLoading,
+} from "./utils"
+import { toggleCreateDestinationForm, toggleSnackbar, toggleSuccessSnackbar } from "./ui"
+
+export const setDestinationInStore = destination => ({
+  type: types.CREATE_DESTINATION_SUCCESS,
+  destination,
+})
+
+export const deleteDestinationFromStore = id => ({
+  type: types.DELETE_DESTINATION_SUCCESS,
+  id,
+})
+
+export const setDestinationsInStore = destinations => ({
+  type: types.FETCH_DESTINATIONS_FAILURE,
+  destinations,
+})
+
+export const editDestinationInStore = id => ({
+  type: types.EDIT_DESTINATION_SUCCESS,
+  id,
+})
+
+export const fetchDestinations = endpointId => (dispatch) => {
+  dispatch(initiateLoading("FETCH_DESTINATIONS"))
+
+  axios.get(`/endpoints/${endpointId}/destinations`)
+    .then((response) => {
+      handleResponse(dispatch, setDestinationsInStore)(response)
+      dispatch(toggleSuccessSnackbar("FETCH_DESTINATIONS"))
+    })
+    .catch((response) => {
+      handleErrorResponse(dispatch, createError("FETCH_DESTINATIONS"))(response)
+      dispatch(toggleSnackbar())
+    })
+}
+
+export const editDestination = (endpointId, destinationId, destData) => (dispatch) => {
+  axios.put(`/endpoints/${endpointId}/destinations/${destinationId}`, destData)
+    .then((response) => {
+      handleResponse(dispatch, setDestinationsInStore)(response)
+      dispatch(toggleSuccessSnackbar("FETCH_DESTINATIONS"))
+    })
+    .catch((response) => {
+      handleErrorResponse(dispatch, createError("FETCH_DESTINATIONS"))(response)
+      dispatch(toggleSnackbar())
+    })
+}
+
+export const createDestination = (endpointId, destinationData) => (dispatch) => {
+  axios.post(`/endpoints/${endpointId}/destinations`, destinationData)
+    .then((response) => {
+      handleResponse(dispatch, setDestinationInStore)(response)
+      dispatch(toggleSuccessSnackbar("Destination was created"))
+      dispatch(toggleCreateDestinationForm())
+    })
+    .catch(handleErrorResponse(
+      compose(dispatch, toggleSnackbar),
+      createError("CREATE_DESTINATION")
+    ))
+}
+
+export const deleteDestination = (endpointId, destinationId) => (dispatch) => {
+  axios.delete(`/endpoints/${endpointId}/destinations/${destinationId}`)
+    .then(() => {
+      dispatch(deleteDestinationFromStore(destinationId))
+      dispatch(toggleSuccessSnackbar("endpoint was deleted"))
+    })
+    .catch(handleErrorResponse(
+      compose(dispatch, toggleSnackbar),
+      createError("DELETE_DESTINATION")
+    ))
+}
