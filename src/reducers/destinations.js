785f12c13431de53d78bf4e183213b2a560b0232
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-24 22:28:01 +0200

    fixed bug with the wrong type name

--- src/reducers/destinations.js
+++ src/reducers/destinations.js
@@ -9,18 +9,19 @@ export default (state = initialState, action) => {
   case types.CREATE_DESTINATION_SUCCESS:
     return {
       ...state,
-      allDestinations: [...state.allDestinations, action.destination],
+      allDestinations: state.allDestinations.concat([action.destination]),
     }
   case types.DELETE_DESTINATION_SUCCESS:
     return {
       ...state,
-      allDestinations: state.allDestinations.filter(dest => dest.id !== action.id)
+      allDestinations: state.allDestinations.filter(dest => dest.id !== action.id),
     }
-  case types.FETCH_CHANNELS_SUCCESS:
+  case types.FETCH_DESTINATIONS_SUCCESS:
     return {
       ...state,
       allDestinations: action.destinations,
     }
-  default: return state
+  default:
+    return state
   }
 }
