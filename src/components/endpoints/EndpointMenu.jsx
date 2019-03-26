d0d536a538007128403658adb0982669bd24a57c
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-26 20:29:30 +0200

    showing confirm dialog after delete endpoint click

--- src/components/endpoints/EndpointMenu.jsx
+++ src/components/endpoints/EndpointMenu.jsx
@@ -1,12 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Menu from "@material-ui/core/Menu"
-import MoreVertIcon from "@material-ui/icons/MoreVert"
-import IconButton from "@material-ui/core/IconButton"
 import ListItemIcon from "@material-ui/core/ListItemIcon"
 import Typography from "@material-ui/core/Typography"
 import AddCircle from "@material-ui/icons/AddCircle"
-import Edit from "@material-ui/icons/Edit"
 import Add from "@material-ui/icons/Add"
 import { withStyles } from "@material-ui/core"
 import MenuItem from "@material-ui/core/MenuItem"
@@ -29,7 +26,9 @@ const styles = () => ({
 })
 
 const EndpointMenu = ({
-  openMenu, anchorEl, handleMenuClose, classes, toggleCreateDestinationForm,
+  // eslint-disable-next-line react/prop-types
+  openMenu, anchorEl, handleMenuClose,
+  classes, toggleCreateDestinationForm, toggleDialog,
 }) => (
   <Menu
     id="simple-menu"
@@ -45,7 +44,7 @@ const EndpointMenu = ({
       <Typography noWrap variant="inherit" color="primary">Add Destination</Typography>
     </MenuItem>
 
-    <MenuItem onClick={() => {}}>
+    <MenuItem onClick={toggleDialog("deleteEndpointDialog")}>
       <ListItemIcon><AddCircle className={classes.deleteIcon} /></ListItemIcon>
       <Typography className={classes.delete} noWrap variant="inherit">Delete Endpoint</Typography>
     </MenuItem>
@@ -58,6 +57,7 @@ EndpointMenu.propTypes = {
   handleMenuClose: PropTypes.func.isRequired,
   toggleCreateDestinationForm: PropTypes.func.isRequired,
   classes: PropTypes.object.isRequired,
+  toggleDialog: PropTypes.func.isRequired,
 }
 
 const mapDispatchToProps = {
--- src/components/endpoints/EndpointTopBar.jsx
+++ src/components/endpoints/EndpointTopBar.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
@@ -8,15 +8,20 @@ import MoreVertIcon from "@material-ui/icons/MoreVert"
 import IconButton from "@material-ui/core/IconButton"
 import EndpointMenu from "./EndpointMenu"
 import TopBar from "../utils/TopBar"
+import ConfirmDialog from "../ConfirmDialog"
+import { deleteEndpoint } from "../../actions/endpoint"
 
 export class EndpointTopBar extends Component {
   state = {
     anchorEl: null,
     openMenu: false,
+    deleteEndpointDialog: false,
   }
 
   static propTypes = {
     title: PropTypes.string.isRequired,
+    match: PropTypes.object.isRequired,
+    deleteEndpoint: PropTypes.func.isRequired,
   }
 
   static defaultProps = {
@@ -26,9 +31,16 @@ export class EndpointTopBar extends Component {
 
   handleMenuClose = () => this.setState({ openMenu: false, anchorEl: null })
 
+  toggleDialog = dialog => () => this.setState(state => ({ [dialog]: !state[dialog] }))
+
+  deleteEndpoint = () => {
+    const { deleteEndpoint, match: { params } } = this.props
+    deleteEndpoint(params.channelId, params.stageId, params.endpointId)
+  }
+
   render() {
     const { title } = this.props
-    const { anchorEl, openMenu } = this.state
+    const { anchorEl, openMenu, deleteEndpointDialog } = this.state
     return (
       <div>
         <TopBar title={title}>
@@ -44,9 +56,19 @@ export class EndpointTopBar extends Component {
               anchorEl={anchorEl}
               openMenu={openMenu}
               handleMenuClose={this.handleMenuClose}
+              toggleDialog={this.toggleDialog}
             />
           </div>
         </TopBar>
+
+        <ConfirmDialog
+          handleCloseWithAction={this.deleteEndpoint}
+          handleClose={this.toggleDialog("deleteEndpointDialog")}
+          open={deleteEndpointDialog}
+          title="delete current endpoint"
+        >
+          Are you sure you want to delete this endpoint? It can`t be undone
+        </ConfirmDialog>
       </div>
     )
   }
@@ -61,9 +83,9 @@ const mapStateToProps = ({ channelsEntities: { entities } }, { match }) => {
   }
 }
 
-const mapDispatchToProps = {
-
-}
+const mapDispatchToProps = dispatch => ({
+  deleteEndpoint: (channelId, stageId, endpointId) => dispatch(deleteEndpoint(channelId, stageId, endpointId)),
+})
 
 export default compose(
   withRouter,
