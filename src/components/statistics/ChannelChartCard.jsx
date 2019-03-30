672c9f9d3274f8a5ad086c2fa18e2aa30e72b546
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-30 12:36:23 +0200

    splitted to separate components

--- src/components/statistics/ChannelChartCard.jsx
+++ src/components/statistics/ChannelChartCard.jsx
@@ -3,22 +3,11 @@ import PropTypes from "prop-types"
 import { connect } from "react-redux"
 import { compose } from "redux"
 import Card from "@material-ui/core/Card"
-import Typography from "@material-ui/core/Typography"
 import { withStyles } from "@material-ui/core"
 import { withRouter } from "react-router-dom"
-import {
-  RadialChart,
-  XYPlot,
-  XAxis,
-  YAxis,
-  ChartLabel,
-  HorizontalGridLines,
-  VerticalGridLines,
-  LineSeries,
-  LineMarkSeries,
-} from "react-vis"
+import GeneralStats from "./GeneralStats"
 import { stageSummary, stageBreakdown, stageTotal } from "../../actions/statistics"
-import CardStatsBlock from "../CardStatsBlock"
+import DynamicTimeChart from "./DynamicTimeChart"
 
 const styles = () => ({
   card: {
@@ -27,64 +16,10 @@ const styles = () => ({
     justifyContent: "space-between",
     width: "100%",
   },
-
-  summaryStats: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-
-  radialCenter: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    backgroundColor: "#fff",
-    borderRadius: "50%",
-    transform: "translate(-50%, -50%)",
-  },
-
-  statsWrap: {
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    padding: "10px 0",
-  },
-
-  statsList: {
-    padding: "0",
-    margin: "0",
-  },
-
-  statsListItem: {
-    margin: "10px 0",
-    position: "relative",
-    textTransform: "capitalize",
-  },
-
-  dot: {
-    width: "10px",
-    height: "10px",
-    position: "absolute",
-    top: "49%",
-    left: "-10%",
-    borderRadius: "50%",
-    transform: "translate(-50%, -50%)",
-  },
-
-  deliverability: {
-    position: "absolute",
-    transform: "translate(-50%, -50%)",
-    top: "50%",
-    left: "50%",
-  },
-
-  radialChartWrap: { position: "relative" },
-  generalInfo: { width: "50%" },
-  radialChart: { position: "relative" },
-  timeChart: { width: "100%", height: "200px" },
-  timeChartWrap: { width: "50%" },
 })
 
 export class ChannelChartCard extends Component {
+
   static propTypes = {
     classes: PropTypes.object.isRequired,
     getStagesStats: PropTypes.func.isRequired,
@@ -114,80 +49,35 @@ export class ChannelChartCard extends Component {
       .map(([name, value], i) => ({ name, angle: value, color: colors[i] }))
   }
 
-  renderChartFields = (chartData) => {
-    const { classes } = this.props
-
-    return chartData.map(({ name, color, angle }) => (
-      <Typography key={color} component="li" className={classes.statsListItem}>
-        <span style={{ backgroundColor: color }} className={classes.dot} />{ name }: { angle }
-      </Typography>
-    ))
-  }
-
   transformTimeChartData = () => {
     const { breakdown } = this.props
 
-    const key = Object.keys(breakdown)[0]
+    const date = Object.keys(breakdown)[0]
+    if (date !== undefined) {
+      return Object.entries(breakdown[date]).map(([time, axis]) => ([
+        Date.parse(`${date} ${time}`),
+        axis["5xx"]
+      ]))
+    }
 
-    // Object.entries(breakdown[key]).
+    return [[Date.parse(new Date()), 0]]
   }
+
 
   render() {
     const { classes, total, summary } = this.props
     const radialChartData = this.transformRadialChartData()
-    const chartFields = this.renderChartFields(radialChartData)
+    const XYChartData = this.transformTimeChartData()
 
     return (
       <Card className={classes.card}>
-        <div className={classes.generalInfo}>
-          <div className={classes.summaryStats}>
-            <CardStatsBlock leftDivider name="Inputs" amount={total.inputs} />
-            <CardStatsBlock name="Destinations" amount={total.destinations} />
-            <CardStatsBlock name="Requests" amount={total.requests} />
-          </div>
-
-          <div className={classes.statsWrap}>
-            <div className={classes.radialChartWrap}>
-              <RadialChart
-                className={classes.radialChart}
-                colorType="literal"
-                data={radialChartData}
-                width={200}
-                height={200}
-                innerRadius={65}
-                radius={90}
-                padAngle={0.04}
-              />
-              <div className={classes.deliverability}>
-                <Typography align="center" variant="h5">{ summary.deliverability }</Typography>
-                <Typography align="center" variant="subtitle1">Deliverability</Typography>
-              </div>
-            </div>
-            <ul className={classes.statsList}>{ chartFields }</ul>
-          </div>
-        </div>
-
-        <div className={classes.timeChartWrap}>
-          <XYPlot xType="time" width={300} height={300}>
-            <HorizontalGridLines />
-            <XAxis />
-            <YAxis />
-            <LineMarkSeries
-              className="linemark-series-example"
-              style={{
-                strokeWidth: "3px",
-              }}
-              lineStyle={{ stroke: "red" }}
-              markStyle={{ stroke: "blue" }}
-              data={[{ x: "", y: 10 }, { x: 2, y: 5 }, { x: 3, y: 10 }]}
-            />
-            <LineMarkSeries
-              className="linemark-series-example-2"
-              curve="curveMonotoneX"
-              data={[{ x: 1, y: 11 }, { x: 1.5, y: 10 }, { x: 3, y: 7 }]}
-            />
-          </XYPlot>
-        </div>
+        <GeneralStats 
+          radialChartData={radialChartData} 
+          total={total} 
+          summary={summary} 
+          chartData={radialChartData} 
+        />
+        <DynamicTimeChart XYChartData={XYChartData} />
       </Card>
     )
   }
@@ -195,7 +85,7 @@ export class ChannelChartCard extends Component {
 
 const mapStateToProps = ({ statistics }) => ({
   total: statistics.stage.total,
-  summary: statistics.stage.summary,
+  summary: {},
   breakdown: statistics.stage.breakdown,
 })
 
