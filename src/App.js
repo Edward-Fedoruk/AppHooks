7014e5378f370708335ed04739f9860cee80a51f
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-31 22:46:14 +0300

    temp placeholder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react"
+import Placeholder from "./components/Placeholder"
+import withNavigation from "./components/withNavigation"
+import webhooks from "./assets/webhooks.svg"
 
 const App = () => (
-  <div className="App">test</div>
+  <div className="App">
+    <Placeholder 
+      imgSrc={webhooks}
+      title="Here will be all statistics"
+
+    />
+  </div>
 )
 
-export default App
+export default withNavigation(App)
